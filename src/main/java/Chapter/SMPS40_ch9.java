8279e922937d5cabb6fab312fdeb2a3f92e14eb9
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-18 17:16:05 +0530

    today commit

---
+++ src/main/java/Chapter/SMPS40_ch9.java
@@ -0,0 +1,34 @@
+package Chapter;
+
+class MyEmployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public void setId(int i){
+        this.id = i;
+    }
+    public int getId(){
+        return id;
+    }
+
+}
+public class SMPS40_ch9 {
+    public static void main(String[] args) {
+    MyEmployee sujit = new MyEmployee();
+    // sujit.id = 45;
+    // sujit.name = "Sujit"; // Throws an error due to private access modifiers
+
+    sujit.setName("Sujit Manmode");
+    System.out.println(sujit.getName());
+
+    sujit.setId(1);
+    System.out.println(sujit.getId());
+    }
+}
