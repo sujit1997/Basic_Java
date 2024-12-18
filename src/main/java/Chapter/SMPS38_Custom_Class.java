0f2dd28eb0fdfb4dacf771e4407d6b202ef1b40b
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-18 14:14:58 +0530

    today commit

---
+++ src/main/java/Chapter/SMPS38_Custom_Class.java
@@ -0,0 +1,38 @@
+package Chapter;
+
+class Employee{
+    int id;
+    int salary;
+    String name;
+    public void printDetails(){
+        System.out.println("My id is "+id);
+        System.out.println("My name is "+name);
+    }
+
+    public int getSalary(){
+    return salary;
+    }
+}
+public class SMPS38_Custom_Class {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class");
+        Employee sujit = new Employee();
+        Employee munna = new Employee();
+        // Setting Properties for sujit
+        sujit.id = 101;
+        sujit.name = "Sujit";
+        sujit.salary = 30;
+
+        // Setting Properties for Munna
+        munna.id = 102;
+        munna.name = "Munna Tripathi";
+        munna.salary = 50;
+        //Printing the attributes
+        //System.out.println(sujit.id);
+        //System.out.println(sujit.name);
+        sujit.printDetails();
+        int salary = sujit.getSalary();
+        System.out.println(salary);
+        munna.printDetails();
+    }
+}
