1ca69e08efa502930c362277b6e0e3b2c09aaf03
sujit1997 <sujitmanmode1997@gmail.com>
2024-12-23 11:09:39 +0530

    today last commit

---
+++ src/main/java/Chapter/SM74_Threat_Prorities.java
@@ -0,0 +1,35 @@
+package Chapter;
+
+class MyThr1 extends Thread{
+    public MyThr1(String name){
+        super(name);
+    }
+    public void run(){
+        int i = 34;
+
+        while(true){
+            // System.out.println("I am a thread");
+            System.out.println("  Thank you : " + this.getName());
+        }
+    }
+}
+public class SM74_Threat_Prorities {
+    public static void main(String[] args) {
+        // Ready Queue : T1 T2 T3 T4 T5
+        MyThr1 t1 = new MyThr1("sujit1");
+        MyThr1 t2 = new MyThr1("sujit2");
+        MyThr1 t3 = new MyThr1("sujit3");
+        MyThr1 t4 = new MyThr1(" sujit4");
+        MyThr1 t5 = new MyThr1("sujit5 (Most Important)");
+        t5.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t3.setPriority(Thread.MIN_PRIORITY);
+        t4.setPriority(Thread.MIN_PRIORITY);
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
+}
