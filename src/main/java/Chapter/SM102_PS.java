0c8a3f9149116a22ddf799e93d02f8e3662cb168
sujit <sujitmanmode1997@gmail.com>
2025-01-24 15:21:44 +0530

    first commit

--- src/main/java/Chapter/SM102_PS.java
+++ src/main/java/Chapter/SM102_PS.java
@@ -1,6 +1,8 @@
 package Chapter;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class SM102_PS {
     public static void main(String[] args) {
@@ -19,6 +21,26 @@ public class SM102_PS {
         }
 
         // Question 2
+        Date d = new Date();
+        System.out.println(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
 
+        //Question 3
+        Calendar c1 = Calendar.getInstance();
+        System.out.println(c1.get(Calendar.HOUR) +":"+(Calendar.MINUTE)+":"+(Calendar.SECOND));
+
+        //Question 4
+        LocalDateTime dt = LocalDateTime.now();
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-mm-yyyy");
+        String mydate = dt.format(df);
+        System.out.println(mydate);
+
+        // Question 5
+        HashSet<Integer> s = new HashSet();
+        s.add(5);
+        s.add(9);
+        s.add(5);
+        s.add(7);
+        s.add(8);
+        System.out.println(s);
     }
 }
