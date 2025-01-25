83a9f761880c8259610f5ca4d0e6da49476039f0
sujit <sujitmanmode1997@gmail.com>
2025-01-25 16:45:52 +0530

    first commit

---
+++ src/main/java/Chapter/SM106_javadoc.java
@@ -0,0 +1,16 @@
+package Chapter;
+/** This class is to demonstrate what javadocsis and how it is used in the java industry
+ * @author sujit
+ * @version 1.0
+ * @since 2025
+ *@see <a href="https://docs.oracle.com/en/java/javase/14/docs/api/index.html">Java Docs</a>
+*/
+public class SM106_javadoc {
+
+    public void add(int a, int b){
+        System.out.println("The sum is: " +a +b);
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/JAVADOCS/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Chapter"},{"l":"org.example"}];updateSearchResults();
