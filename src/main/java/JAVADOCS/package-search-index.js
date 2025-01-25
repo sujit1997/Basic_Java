ca06dfd9b52f542f07de206b6d9f89dee56536db
sujit <sujitmanmode1997@gmail.com>
2025-01-25 16:48:55 +0530

    first commit

--- src/main/java/JAVADOCS/package-search-index.js
+++ src/main/java/JAVADOCS/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Chapter"},{"l":"org.example"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Chapter"}];updateSearchResults();
